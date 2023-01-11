36246f868172664807f747d4e4716ef69817fe35
Thiago <thiago-470@outlook.com>
2023-01-11 20:39:29 -0300

    [GP4]- continuação do req 4

---
+++ src/redux/actions/index.js
@@ -0,0 +1,9 @@
+// export const ADD_PERSONAL_INFO = 'ADD_PERSONAL_INFO';
+
+// export const addPersonal = (personalInfo) => {
+//   console.log('chamou action');
+//   return {
+//     type: ADD_PERSONAL_INFO,
+//     payload: { ...personalInfo },
+//   };
+// };
