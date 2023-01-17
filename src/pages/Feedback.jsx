cdf5fe7ae610ab2a3d2a68d3b49387a7135f73d4
PedrovRamos <pvramos.cunha@gmail.com>
2023-01-16 23:32:37 -0300

    Adicionando projeto com requisitos 11, 12 e 13 concluidos

--- src/pages/Feedback.jsx
+++ src/pages/Feedback.jsx
@@ -1,12 +1,34 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 import Header from './Header';
 
 class Feedback extends React.Component {
+  state = {
+    feedMsg: '',
+  };
+
+  componentDidMount() {
+    this.feedMsgGen();
+  }
+
+  feedMsgGen() {
+    const { assertions } = this.props;
+    const mN = 3;
+    if (assertions < mN) {
+      this.setState({ feedMsg: 'Could be better...' });
+    } else {
+      this.setState({ feedMsg: 'Well Done!' });
+    }
+  }
+
   render() {
+    const { feedMsg } = this.state;
     return (
       <>
         <Header />
+        <div data-testid="feedback-text">{feedMsg}</div>
         <Link to="/">
           <button data-testid="btn-play-again" type="button">
             Play Again
@@ -17,4 +39,12 @@ class Feedback extends React.Component {
   }
 }
 
-export default Feedback;
+Feedback.propTypes = {
+  assertions: PropTypes.number,
+}.isRequired;
+
+const mapStateToProps = (globalState) => ({
+  assertions: globalState.player.assertions,
+});
+
+export default connect(mapStateToProps)(Feedback);
